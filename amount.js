165dcd3b80b9b1329e80eaecacce5dbf9b5a4eb2
Maxim Kalenich <maxim.kalenich@gmail.com>
2013-12-25 20:02:48 +0400

    Choose base algorithm

--- amount.js
+++ amount.js
@@ -1,32 +1,33 @@
-var Amount = function( value, cur_name, rate ) {
-  if (typeof value !== "number")
-    throw new Error("Value must be a number!");
-  if (typeof cur_name !== "string") 
-    throw new Error("Currency must be a string!");
+var Amount = function(value, currency, rate) {
+  if ( !! this.window)
+    return new arguments.callee(value, currency, rate);
   this.value = value;
-  this.currency = cur_name;
+  this.currency = currency;
   this.rate = rate;
-  this.toString = function() { return [this.value, this.currency].join(" "); };
-  this.valueOf = function() { return this.value * this.rate; };
-  this.add = function( amount ) { this.value += amount / this.rate; return this; };
-  this.odd = function( amount ) { this.value -= amount / this.rate; return this; };
-  this.to_ = function( cur ) { return Amount.set(this.valueOf() * cur.rate, cur.currency, cur.rate) };
+  this.toString = function() {
+    return [this.value, this.currency].join(" ");
+  };
+  this.valueOf = function() {
+    return this.value * this.rate;
+  };
+  this.add = function(amount) {
+    this.value += amount / this.rate;
+    return this;
+  };
+  this.odd = function(amount) {
+    this.value -= amount / this.rate;
+    return this;
+  };
+  this.to_ = function(currency) {
+    console.log(this);
+  };
 };
-Amount.set = function( value, cur_name, rate ) { return new Amount(value, cur_name, rate); };
-Amount.set_currency = function( name, rate ) {
+Amount.set_currency = function(name, rate) {
   var self = this;
-  self[name] = function( value ) { self.call(this, value, name, rate); };
-  self[name].prototype = Amount.set(0, "");
-  window[name] = self[name].set = function( value ) { return new self[name]( value ); };
+  self[name] = function(value) {
+    if ( !! this.window || this === self)
+      return new self[name](value);
+    self.call(this, value, name, rate);
+  };
+  windows[name] = self[name].prototype = new self(0, name);
 };
-
-var small_amount = Amount.set(100, "USD", 1);
-console.log(small_amount instanceof Amount, small_amount);
-Amount.set_currency("USD", 1);
-
-Amount.set_currency("RUB", 0.03333);
-var smaller_amount = Amount.RUB.set(100);
-console.log(smaller_amount instanceof Amount.RUB, smaller_amount instanceof Amount, smaller_amount);
-
-var some = RUB(4000);
-console.log(some.to_(small_amount));
