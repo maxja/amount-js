f859660d2789c20b4e89923415166e3b0e869ca1
Maxim Kalenich <maxim.kalenich@gmail.com>
2013-12-26 11:52:13 +0400

    update methods

--- amount.js
+++ amount.js
@@ -1,15 +1,9 @@
 var Amount = function(value, currency, rate) {
-  if ( !! this.window)
+  if ( !!this.window )
     return new arguments.callee(value, currency, rate);
   this.value = value;
   this.currency = currency;
   this.rate = rate;
-  this.toString = function() {
-    return [this.value, this.currency].join(" ");
-  };
-  this.valueOf = function() {
-    return this.value * this.rate;
-  };
   this.add = function(amount) {
     this.value += amount / this.rate;
     return this;
@@ -19,15 +13,26 @@ var Amount = function(value, currency, rate) {
     return this;
   };
   this.to_ = function(currency) {
-    console.log(this);
+    return new Amount(
+      this / currency.rate, 
+      currency.currency, 
+      currency.rate
+    );
+  };
+  this.toString = function() {
+    return [this.value, this.currency].join(" ");
+  };
+  this.valueOf = function() {
+    return this.value * this.rate;
   };
 };
 Amount.set_currency = function(name, rate) {
   var self = this;
   self[name] = function(value) {
-    if ( !! this.window || this === self)
+    if ( !!this.window || this === self )
       return new self[name](value);
     self.call(this, value, name, rate);
   };
-  windows[name] = self[name].prototype = new self(0, name);
+  self[name].prototype = new self(0, name);
+  return self[name];
 };
